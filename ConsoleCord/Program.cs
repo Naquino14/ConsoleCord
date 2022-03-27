26702bb26db96faaa2acdd15a718ad5634382be8
Nathaniel Aquino <37340195+Naquino14@users.noreply.github.com>
2022-03-27 17:17:19 -0400

    Create Project

---
+++ ConsoleCord/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using c = System.Console;
+
+namespace ConsoleCord
+{
+    public class Program
+    {
+        public static void Main(string[] vs)
+        {
+
+        }
+    }
+}
